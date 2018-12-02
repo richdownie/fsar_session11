ba122b526112882f32bfbd13a2f0cc087e40ddaf
Rich Downie <rich@blackboxd.com>
2018-12-02 08:39:56 -0500

    cleanup cukes

--- features/step_definitions/widget_steps.rb
+++ features/step_definitions/widget_steps.rb
@@ -1,16 +1,3 @@
-Given /I am on the fsar widgets page/ do
-  @widgets_index.visit
-end
-
-Then /I verify widgets display on the page/ do
-  @browser.find_element(:id, 'widgets').displayed?
-end
-
-Then /I verify the widgets table has "(.*)" rows/ do |row_count|
-  rows = @browser.find_elements(css: '#widgets_table tr').count - 1
-  expect(rows).to eql(row_count.to_i)
-end
-
 Given(/^I am on the new widget page$/) do
   @widget_new.visit
 end
--- features/widgets.feature
+++ features/widgets.feature
@@ -1,10 +1,5 @@
 Feature: Advanced FSAR Widget Scenarios
 
-  Scenario: Verify widgets display on the page
-    Given I am on the fsar widgets page
-    Then I verify widgets display on the page
-    Then I verify the widgets table has "3" rows
-
   Scenario: Fill out the new widget form
     Given I am on the new widget page
     And I fill out the new widget form
